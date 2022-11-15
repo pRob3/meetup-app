9f73e6bc5be13c3419401a64ad5904dc925208bf
pRob3 <skene@hotmail.com>
2022-11-15 08:22:13 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
+import MainNavigation from './components/layout/MainNavigation';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <MainNavigation />
+        <Routes>
+          <Route path='/' exact element={<AllMeetupsPage />} />
+          <Route path='/new-meetup' element={<NewMeetupPage />} />
+          <Route path='/favorites' element={<FavoritesPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
