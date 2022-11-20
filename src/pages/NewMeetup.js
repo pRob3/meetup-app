ccba85cd006017353ab5d9beaaf77fe5b3f99825
pRob3 <skene@hotmail.com>
2022-11-20 17:38:21 +0100

    formAdd new meetup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,7 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import NewMeetupForm from '../components/meetups/NewMeetupForm';
+
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(data) {
+    fetch(
+      'https://react-test-backend-1da08-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    ).then(() => {
+      navigate('/');
+    });
+  }
+
   return (
     <section>
-      <h1>New Meetup Page</h1>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
