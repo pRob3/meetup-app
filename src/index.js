c939469a73a7f6bde9356fe5acebcaeaab7220f6
pRob3 <skene@hotmail.com>
2022-11-21 18:18:58 +0100

    Using context In components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import { createRoot } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { FavoritesContextProvider } from './store/favorites-context';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(
+  <FavoritesContextProvider>
+    <App />
+  </FavoritesContextProvider>
+);
